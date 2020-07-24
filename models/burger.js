01202183dc4c7cd2a82b034d9cc7bc55ee58c865
Evan Boswood <evan@boswood.net>
2020-07-24 16:47:00 -0500

    created burger model

--- models/burger.js
+++ models/burger.js
@@ -1,3 +1,27 @@
 // Add Dependencies
 const orm = require('../config/orm.js');
 
+const burger = {
+    all: function(cb){
+        orm.all('burgers', res => {
+            cb(res);
+        });
+    },
+    create: function(cols, vals, cb){
+        orm.insert('burgers', cols, vals, res => {
+            cb(res);
+        });
+    },
+    update: function(objColVals, condition, cb){
+        orm.update('burgers', objColVals, condition, res => {
+            cb(res);
+        })
+    },
+    delete: function(condition, cb){
+        orm.delete('burgers', condition, res => {
+            cb(res);
+        });
+    }
+};
+
+module.exports = burger;
