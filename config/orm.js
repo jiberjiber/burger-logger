81d39a234276809ebf27332f53b6c919a180f707
Evan Boswood <evan@boswood.net>
2020-07-24 17:05:26 -0500

    orm object functions updated

--- config/orm.js
+++ config/orm.js
@@ -1,3 +1,45 @@
 // Add Dependencies
 const connection = require('../config/connection.js');
+const { query } = require('express');
 
+
+// ORM Object
+const orm = {
+    all: function(table, cb){
+        var queryString = `SELECT * FROM ${table};`;
+        connection.query(queryString, function(err, res){
+            if(err) throw err;
+
+            cb(res);
+        });
+    },
+    create: function(table, val, cb){
+        var queryString = `INSERT INTO ${table}(burger)`;
+        queryString += ` VALUES (${val});`;
+
+        console.log(`Query: ${queryString}`);
+
+        connection.query(queryString, function(err, result){
+            if(err) throw err;
+
+            cb(res);
+        });
+    },
+    update: function(table, objColVals, condition, cb){
+        console.log(`Update called!`);
+    },
+    delete: function(table, condition, cb){
+        var queryString = `DELETE FROM ${table}`;
+        queryString += ` WHERE burgers = ${condition}`;
+
+        connection.query(queryString, function(err, result){
+            if(err) throw err;
+
+            cb(res);
+        });
+    }
+};
+
+
+// Set export
+module.exports = orm;
